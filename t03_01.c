5fbfabcac9bf9fdbede49360d770445c82f1b565
Cardolansinaga <143679048+Cardolansinaga@users.noreply.github.com>
2024-02-06 20:50:48 +0700

    t03_01.c

--- t03_01.c
+++ t03_01.c
@@ -1,11 +1,31 @@
-// NIM - NAMA
+// 12S23006_Cardolan Sinaga
 
 #include <stdio.h>
 
-int main(int _argc, char **_argv)
-{
+int main() {
+    int n, i, nilai;
+    int min = 101; // Inisialisasi nilai minimum dengan nilai yang tidak mungkin dicapai
+    int max = -101; // Inisialisasi nilai maksimum dengan nilai yang tidak mungkin dicapai
 
-  // your code here
+    // Menerima masukan jumlah baris dan nilai-nilai
+    scanf("%d", &n);
 
-  return 0;
+    // Menerima nilai-nilai dan mencari nilai minimum dan maksimum
+    for (i = 0; i < n; i++) {
+        scanf("%d", &nilai);
+
+        // Memperbarui nilai minimum dan maksimum jika ditemukan nilai yang lebih kecil atau lebih besar
+        if (nilai < min) {
+            min = nilai;
+        }
+        if (nilai > max) {
+            max = nilai;
+        }
+    }
+
+    // Menampilkan nilai minimum dan maksimum
+    printf("%d\n", min);
+    printf("%d\n", max);
+
+    return 0;
 }
